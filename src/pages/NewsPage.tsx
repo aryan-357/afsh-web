8f0a122e58b0a4fb112888b1b23f22786e430d35
AryanRajput-coder <kr357403@gmail.com>
2026-01-08 14:12:14 +0530

    bug fix

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -42,7 +42,7 @@ const NewsPage = () => {
     };
 
     useEffect(() => {
-        fetch(`${API_URL}/api/posts?populate[0]=*&populate[1]=authors`)
+        fetch(`${API_URL}/api/posts?populate=category,coverContent,authors`)
             .then(res => {
                 if (!res.ok) {
                     if (res.status === 404) throw new Error("Posts endpoint not found.");
